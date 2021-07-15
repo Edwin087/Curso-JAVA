4fb28186e2b0f6feba972abd8a59de718051bbb4
Edwin087 <calleojeda4@gmail.com>
2021-07-15 16:58:15 -0500

    Curso:java

---
+++ src/VariablesEdad.java
@@ -0,0 +1,18 @@
+
+public class VariablesEdad {
+
+	public static void main(String[] args) {
+
+		int edad = 35; //declaracion e  iniciacion  en una unica linea
+		
+		/*la siguiete  imprime en la consola 
+		 * el valor de la varible edad */
+		
+		System.out.println(edad);
+		
+		edad=25;
+		System.out.println(edad);
+
+	}
+
+}
